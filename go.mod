ae14c0affa8f12c900aceb23b95952158be32c17
nadaaten <nada@atentec.com>
2024-04-25 16:09:21 +0000

    check is not null or pointer

--- go.mod
+++ go.mod
@@ -2,11 +2,16 @@ module github.com/atenteccompany/goutilities
 
 go 1.22.0
 
+require github.com/fatih/color v1.16.0
+
 require (
 	github.com/bytedance/sonic v1.11.4 // indirect
 	github.com/cloudwego/base64x v0.1.0 // indirect
 	github.com/cloudwego/iasm v0.0.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- refl.go
+++ refl.go
@@ -1,9 +1,7 @@
 package goutilities
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 )
 
 func PriVal(val interface{}) (pri int64, err error) {
@@ -52,77 +50,4 @@ func AnyToInt64(val interface{}) (pri int64, err error) {
 	}
 
 	return pri, nil
-}
-
-// ---------------------------------------------------------------------------------------------------------
-type fieldInfo struct {
-	Name  string
-	Type  string
-	Index int
-}
-
-func PrintWithReflVal(v reflect.Value) {
-	t := reflect.TypeOf(v.Interface())
-	PrintWithReflType(t)
-}
-
-func PrintWithReflType(t reflect.Type) {
-	//Print type statistics
-	fmt.Print("\t\t\tType Statistics\n\n")
-	fmt.Printf("%-20s | %-20s | \n%-20s | %-20d |", "Type", "Number of Fields", t.Name(), t.NumField())
-	//Print type fields
-	fmt.Print("\n\n\t\t\tType Fields\n\n")
-	fmt.Printf("%-20s | %-20s | %-5s | Tags\n", "Field Name", "Type", "Index")
-	fields := getFieldInfo(t)
-	for _, field := range fields {
-		printFieldInfo(t, field)
-	}
-
-	fmt.Println("------------------------------------------------------------------------------------------------------")
-}
-
-func printFieldInfo(t reflect.Type, field fieldInfo) {
-	fmt.Printf("%-20s | %-20s | %-5d | ", field.Name, field.Type, field.Index)
-
-	// Get all tags
-	tag := t.Field(field.Index).Tag
-	tagList := splitTags(tag)
-	// Print tags
-	for tagName, tagValue := range tagList {
-		fmt.Printf("%s: %s | ", tagName, tagValue)
-	}
-
-	fmt.Println()
-}
-
-func splitTags(tag reflect.StructTag) map[string]string {
-	tags := make(map[string]string)
-
-	for _, tagName := range strings.Split(string(tag), " ") {
-		if tagName == "" {
-			continue
-		}
-		parts := strings.SplitN(tagName, ":", 2)
-		key := parts[0]
-		value := strings.Trim(parts[1], `"`)
-		tags[key] = value
-	}
-
-	return tags
-}
-
-func getFieldInfo(t reflect.Type) []fieldInfo {
-	var fields []fieldInfo
-	for i := 0; i < t.NumField(); i++ {
-		fieldType := t.Field(i).Type
-		fieldName := t.Field(i).Name
-
-		fields = append(fields, fieldInfo{
-			Name:  fieldName,
-			Type:  fieldType.Name(),
-			Index: i,
-		})
-
-	}
-	return fields
 }
